fe02253bb1ae8dc3c1453747d5be2d19a3b11deb
Trung Lê <thanhtrunghk7164@gmail.com>
2021-06-10 12:40:02 +0700

    Xu ly rotate

--- client/src/components/Tetris.js
+++ client/src/components/Tetris.js
@@ -18,7 +18,7 @@ const Tetris = () => {
   const [dropTime, setDropTime] = useState(null);
   const [gameOver, setGameOver] = useState(false);
 
-  const [player, updatePlayerPos, resetPlayer] = usePlayer();
+  const [player, updatePlayerPos, resetPlayer, playerRotate] = usePlayer();
   const [stage, setStage] = useStage(player, resetPlayer);
 
   console.log('re-render');
@@ -63,6 +63,8 @@ const Tetris = () => {
         movePlayer(1);
       } else if (keyCode === 40) {
         dropPlayer();
+      } else if (keyCode === 38) {
+        playerRotate(stage, 1);
       }
     }
   }
